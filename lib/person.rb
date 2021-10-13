6620937c0ce8821ed923dc372482e644eee6ead0
Yewei <yeweichen1@gmail.com>
2021-10-13 11:29:10 -0500

    commit tree 270c5b272fbc4cdda59f29a200d899dd0645167e

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+
+
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+ricky = Person.new
+ricky.talk
+ricky.walk
